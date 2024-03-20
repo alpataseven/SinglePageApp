ff93af848e5bea56c4fccecab6ff30e6d88cf994
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-03-20 03:40:13 +0300

    finish..

--- main.js
+++ main.js
@@ -3,25 +3,53 @@ console.log("window.location.port", window.location.port)
 console.log("window.location.host, window.location.host")
 console.log("window.location.hostname", window.location.hostname)
 
+const app = document.querySelector('#app')
+
 const hashLinks = document.querySelectorAll('.hash-route-link')
 
 const routes = {
     "": {
-        title: "Ana Sayfa"
+        title: "Ana Sayfa",
+        isApi: false,
+        data: "./home.html",
     },
     "#todos": {
-        title: "Todo List"
+        title: "Todo List",
+        isApi: true,
+        data: "https://jsonplaceholder.typicode.com/todos/",
     },
     "#about": {
-        title: "About Us"
+        title: "About Us",
+        data: "./about-us.html",
+        isApi: false,
     },
     "#contact": {
-        title: "Contact"
+        title: "Contact",
+        data: "contact.html",
+        isApi: false,
     },
 }
 
 function checkRoute(hash = window.location.hash) {
     document.title = routes[hash].title
+    const dataUrl = routes[hash].data
+    isApi = routes[hash].isApi
+    if (isApi) {
+        app.innerHTML = ""
+        const createUl = document.createElement('ul')
+        app.append(createUl)
+        
+        fetch(dataUrl).then(res => res.json()).then(data => {
+            data.forEach(item => {
+                const createLi = document.createElement('li')
+                createLi.innerHTML = item.title
+                createUl.append(createLi)
+            })
+        })
+    } else if (!isApi) {
+        fetch(dataUrl).then(res => res.text()).then(data => app.innerHTML = data)
+    }
+    
 }
 
 hashLinks.forEach(item => item.addEventListener("click", (event) => {
